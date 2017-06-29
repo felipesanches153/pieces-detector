0666b9e88bbdfdd00120b0a840a17c2f76c71c43
Felipe Sanches <sanchesfelipe153@gmail.com>
2017-06-29 20:42:06 -0300

    First commit
    Configuring OpenCV

---
+++ app/src/main/java/io/github/felipesanches153/pieces/detector/MainActivity.kt
@@ -0,0 +1,23 @@
+package io.github.felipesanches153.pieces.detector
+
+import android.app.Activity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.opencv.android.OpenCVLoader
+
+class MainActivity : Activity() {
+
+    companion object {
+
+        @JvmField
+        val TAG = "MainActivity"
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main_text_view.text = if (OpenCVLoader.initDebug()) "OpenCV OK" else "OpenCV not OK"
+    }
+
+}
